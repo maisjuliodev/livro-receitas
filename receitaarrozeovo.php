f0b37be1f062468ab01592898b1ac8434f833ee8
julio ferrao <julioferrao1979@gmail.com>
2022-01-23 10:32:41 -0300

    primeiro commit desse projeto do banco inter das aulas de git e github

---
+++ receitaarrozeovo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>php oo do curso do cfb do youtube.</title>
+	</head>	
+	<body>
+		<h6>Ingredientes</h6>
+			<h4>arroz e ovo</h4>
+			<p>*2 xicaras de arroz</p>
+			<p>*2 ovos</p>
+		<h6>Modo de preparo</h6>
+			<p>faz o arroz com cebola e alho refogados</p>
+			<p>faz o ovo</p>
+			<p>Mistura os dois</p>
+	</body>	
+</html>	
+
