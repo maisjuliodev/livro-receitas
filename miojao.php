e1cd9704a71b965c47decf283e831cc46c7a3cbe
julio ferrao <julioferrao1979@gmail.com>
2022-01-25 21:40:06 -0300

    adiciona o miojao

---
+++ miojao.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>php oo do curso do cfb do youtube.</title>
+	</head>	
+	<body>
+		<h6>Miojão</h6>
+		<h4>Ingredientes</h4>
+			*miojo
+			*pó do miojo
+		<h4>Modo de Preparo</h4>
+			Com o miojo feito na panela, já molenga, você pega o pó do miojo e joga em cima e mistura. Simples assim. Está pronto.
+	</body>
+</html>
